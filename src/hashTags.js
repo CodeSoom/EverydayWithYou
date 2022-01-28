0f736d10a5f047e238231be5738c8e549e7e6c70
Superduper-India <sunyoung1542@gmail.com>
2022-01-28 16:01:18 +0900

    Collect hashTage: conditions, regions, categories

---
+++ src/hashTags.js
@@ -0,0 +1,18 @@
+// ToDo: 콜렉션 더 추가
+const conditions = [
+  { id: 1, name: '#혼밥' },
+];
+
+const regions = [
+  { id: 1, name: '#서울 송파' },
+];
+
+const categories = [
+  { id: 1, name: '#면' },
+];
+
+export {
+  conditions,
+  regions,
+  categories,
+}
