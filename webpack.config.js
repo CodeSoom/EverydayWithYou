9a48d639f34b4a18c485ff59ea4684179da97a60
Superduper-India <89244209+Superduper-India@users.noreply.github.com>
2022-02-11 17:25:39 +0900

    Change settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.jsx',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
+  entry: path.resolve(__dirname, 'src/index.jsx'),
   module: {
     rules: [
       {
